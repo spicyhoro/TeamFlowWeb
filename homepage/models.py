d424ae8dda103c30a2d9d6e3f44a1afee96d9005
spicyhoro <spicyhoro777@gmail.com>
2019-12-01 21:10:47 +0900

    처음

---
+++ homepage/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db import models
+from django.conf import settings
+from django.urls import reverse
+
+class Post(models.Model):
+     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+     nickname = models.CharField(max_length=100, verbose_name="닉네임")
+     tags = models.CharField(max_length=50, blank=True)
+     content = models.TextField(verbose_name="설명")
+     created_at = models.DateTimeField(auto_now_add=True)
+     updated_at = models.DateTimeField(auto_now=True)
+
+
---
+++ homepage/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def post_list(request):
+    qs = Post.objects.all()
+    return render(request, 'homepage/post_list.html', {"post_list": qs} )
+
+# Create your views here.
